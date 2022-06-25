675ac7dc9dfc59580a3b6152ccb5f8accbbf0030
Simo Dida <simodida383@gmail.com>
2022-06-25 23:56:35 +0100

    Pricing

---
+++ src/components/images-float/images-float.js
@@ -0,0 +1,11 @@
+import "./images-float.css";
+
+function ImgFloat() {
+    return (
+        <div className="img-float">
+            <img src="../images/bg-top.svg" />
+            <img src="../images/bg-bottom.svg" />
+        </div>
+    );
+}
+export default ImgFloat;
